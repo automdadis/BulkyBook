50b35c0aa285a7b1bc6a45660e4eccdaa4422c8b
Δαδής Ηλίας < @DESKTOP-E39H2Q5>
2023-01-06 11:36:47 +0200

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    //Είναι ο πίνακας Category μέσα στην db
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime { get; set; }  = DateTime.Now; 
+
+    }
+}
